0747ea81d08df2a993866b573acc1d330823f8c9
ROMANETTI Flavien <Flavien.Romanetti@bordeaux-inp.fr>
2025-12-18 11:00:12 +0100

    refacto isPrime

--- code.py
+++ code.py
@@ -18,11 +18,11 @@ def getListMax(nombres,max):
             listMax.append(nombre)
     return listMax
 
-def p(n):
-    if n<2:
-        return False
-    for i in range(2,int(math.sqrt(n))+1):
-        if n%i==0:
+def isPrime(nombre):
+    if nombre<2:
+        return True
+    for diviseur in range(2,int(math.sqrt(nombre))+1):
+        if nombre%diviseur==0:
             return False
     return True
 
