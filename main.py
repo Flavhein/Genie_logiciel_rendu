29a3058cf74e8d9668664e609e4577bb61ec0d60
ROMANETTI Flavien <Flavien.Romanetti@bordeaux-inp.fr>
2025-12-18 11:25:52 +0100

    del unuse var and improve print

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fct_math import *
 
-profil={'nom':'Jean','age':25,'ville':'Paris'}
 nombres=[12,5,8,130,44,7,19,23,2]
 
-print(moyenne(nombres))
-print(getListSupTo(nombres,10))
-print(listToSquare([1,2,3,4,5]))
-print(isPrime(17))
-print(pgdc(48,18))
+print(f"moyenne : {moyenne(nombres)}")
+print(f"getListSupTo : {getListSupTo(nombres,10)}")
+print(f"listToSquare : {listToSquare([1,2,3,4,5])}")
+print(f"isPrime : {isPrime(17)}")
+print(f"pgdc(48,18) : {pgdc(48,18)}")
