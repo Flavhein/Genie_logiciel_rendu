9bcd1676f264b6e579205685748760b7cb3ddeb3
ROMANETTI Flavien <Flavien.Romanetti@bordeaux-inp.fr>
2025-12-19 11:10:52 +0100

    tdd from begining

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,11 +1,5 @@
 function isValide(mdp) {
-    if (mdp.length < 8){
-        return false
-    }
-    if (!/\d/.test(mdp)){
-        return false
-    }
-    return true
+    return true;
 }
 
 module.exports = isValide;
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,10 +1,10 @@
 const isValide = require('./passwordValidator');
 
 
-test('bonjour1 is at least 8 chars', () => {
+test('bonjour1 is at least 8 chars, one letter and one digit', () => {
   expect(isValide("bonjour1")).toBe(true);
 });
 
-test('124562562456 has at least 1 digit', () => {
-    expect(isValide("124562562456")).toBe(true);
-});
+
+
+
